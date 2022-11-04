d58d270d509761e40d76f4351f48d6c25ff15509
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2022-11-04 18:55:48 +0000

    add check neighbours, main and makefile

---
+++ parallel/check_neighbours.c
@@ -0,0 +1,28 @@
+#include "declarations.h"
+
+CELL neighbour;
+
+float check_neighbours(CELL **world, int row, int col) {
+    float chance = 0.0;
+    int row_offset[8] = { 0,  0, -1,  1,  1,  1, -1, -1};
+    int col_offset[8] = {-1,  1,  0,  0, -1,  1, -1,  1};
+
+    for(int i = 0; i < NEIGHBOUR_COUNT; i++) {
+        neighbour = world[(row + row_offset[i] % SIM_SIZE + SIM_SIZE) % SIM_SIZE][(col + col_offset[i] % SIM_SIZE + SIM_SIZE) % SIM_SIZE];
+
+        if (neighbour.status == EXPOSED) {
+            chance += EXPOSED_INFECTION_CHANCE;
+        } else if (neighbour.status == INFECTED) {
+            chance += INFECTED_INFECTION_CHANCE;
+        }
+
+        #if (DEBUG > 2)
+            printf("%2d ", neighbour)
+        #endif
+    }
+    #if (DEBUG > 2)
+        printf("Chance: %d\n", chance)
+    #endif
+
+    return chance;
+}
---
+++ parallel/main.c
@@ -0,0 +1,57 @@
+#include "declarations.h"
+
+// todo move to declarations
+// const int MAX_THREADS = 1024;
+
+// long thread_count;
+// long long n;
+// double sum;
+
+int main() {
+    int i;
+    CELL *world[SIM_SIZE];
+    CELL *newWorld[SIM_SIZE];
+
+    FILE *population_file = fopen("population.csv", "w");
+
+    srand((unsigned int)time(NULL));
+
+    // add header to csv
+    fprintf(population_file, "Generation,Population Susceptible,Population Exposed,Population Infected,Population Removed\n");
+
+    // initialise world
+    initialize_world(world, newWorld);
+
+    // output generation 0
+    output_to_file(world, 0, population_file);
+
+    // insert threads
+
+    // long       thread;  /* Use long in case of a 64-bit system */
+    // pthread_t* thread_handles;
+
+    // thread_handles = (pthread_t*) malloc(thread_count*sizeof(pthread_t));
+
+    // for (thread = 0; thread < thread_count; thread++)
+    //     pthread_create(&thread_handles[thread], NULL, thread_func, (void*)thread);
+
+    // for (thread = 0; thread < thread_count; thread++)
+    //     pthread_join(thread_handles[thread], NULL);
+
+    // end of program
+
+    printf("See /output and population.csv for output\n");
+
+    // close files and free memory
+    fclose(population_file);
+
+    for (i = 0; i < SIM_SIZE; i++)
+    {
+        free(world[i]);
+        free(newWorld[i]);
+    }
+
+    // free(thread_handles);
+
+    return 0;
+}
